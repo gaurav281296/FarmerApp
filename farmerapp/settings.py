448199aae2a61a1b24bdd0d4086ca4a0cde04556
Gaurav Agarwal <gaurav.agarwal28.12.96@gmail.com>
2019-05-11 03:56:59 +0530

    djangorestframework up and running.

--- farmerapp/settings.py
+++ farmerapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')oq6#x%kynn2(deadxs&f#blu(5+(yrt+cn%*2gyp&1jh-8min'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
